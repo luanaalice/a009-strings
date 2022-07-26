9f8a39cc35a5bdd0fda86a38769f3eddb8b58bbb
unknown <lluanacorreia@gmail.com>
2022-07-26 18:20:44 -0300

    trabalhoOk

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString. trim())
+console.log(minhaString.length)
+
+const novaString= "Eu não sou supersticioso, mas sou um pouco ________. ".replace("________", "sticioso");
+console.log(novaString)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+
+const frase= "Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair"
+const novaFrase="Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair". replace ("verde","rosa"). replace( "azul", "laranja") 
+console.log(novaFrase)
+
+const gato= "mas não deixe o gato sair ";
+const gatoToUpperCase= gato.toUpperCase();
+console.log(gatoToUpperCase)
+
